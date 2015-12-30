b00d3a8155984241a5017a582a5df7773437af08
slyrz <slyrz@users.noreply.github.com>
2015-12-30 13:26:28 +0100

    formatting

--- feature/plugin/keras.py
+++ feature/plugin/keras.py
@@ -2,7 +2,7 @@ import numpy
 
 from ..feature import ModelTransform
 
-class Transform(ModelTransform):
 
+class Transform(ModelTransform):
     def transform(self, array):
         return self.model.predict(numpy.array(array))
