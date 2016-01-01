d074995f8ce5a62104525b1f3cfed10ace12c3bc
slyrz <slyrz@users.noreply.github.com>
2016-01-01 11:59:40 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name="feature",
+      version="0.1",
+      url="https://github.com/slyrz/feature",
+      description="Easy feature engineering.",
+      long_description=open('README.md').read(),
+      packages=['feature', 'feature.plugin'],
+      license='MIT')
